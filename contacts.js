75517e65f9522fef6dea4ab8f66593d5c60c889b
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2023-02-07 20:11:22 +0200

    job is done

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve("contacts.json");
+
+
+async function getData() {
+  const res = await fs.readFile("db/contacts.json");
+  return JSON.parse(res);
+}
+
+async function writeData(data) {
+  const stringifiedData = JSON.stringify(data);
+  await fs.writeFile("db/contacts.json", stringifiedData);
+}
+
+async function listContacts() {
+  const data = await getData();
+  console.log(data);
+}
+
+async function getContactById(contactId) {
+  const data = await getData();
+  const contactById = data.find(({ id }) => id === contactId);
+  return console.log(contactById);
+}
+
+async function removeContact(contactId) {
+  const data = await getData();
+  const unremovedContacts = data.filter(
+    ({ id }) => !(id === contactId)
+  );
+  writeData(unremovedContacts);
+  return console.log(`contact with ${contactId} has been removed`);
+}
+
+async function addContact(name, email, phone) {
+  const data = await getData();
+  const upgradedData = [
+    ...data,
+    { id: Date.now().toString(), name: name, email: email, phone: phone },
+  ];
+  writeData(upgradedData);
+}
+
+module.exports = { listContacts, addContact, removeContact, getContactById};
